ce94a56c1db7e9b302caef0d5a1b917b4e0cd4c3
Vijay Ganesh H <vijay.g1@samsung.com>
2020-10-24 18:15:56 +0530

    Modify react api calls to remove host and port

--- client/src/actions/employeeActions.js
+++ client/src/actions/employeeActions.js
@@ -4,7 +4,7 @@ import _ from "lodash";
 
 export const fetchEmployees = () => async (dispatch) => {
   try {
-    const res = await axios.get(`http://127.0.0.1:5000/employees`);
+    const res = await axios.get(`/employees`);
     const employees = _.get(res, 'data', [])
     dispatch({
       type: FETCH_EMPLOYEES_SUCCESS,
@@ -19,7 +19,7 @@ export const fetchEmployees = () => async (dispatch) => {
 
 export const createEmployee = (empData) => async (dispatch) => {
   try {
-    const res = await axios.post(`http://127.0.0.1:5000/employee/`, empData);
+    const res = await axios.post(`/employee/`, empData);
     const employee = _.get(res, 'data', {})
     dispatch({
       type: CREATE_EMPLOYEE_SUCCESS,
